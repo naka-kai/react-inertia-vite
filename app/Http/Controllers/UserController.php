2a316293d8fe84afe558b6f199378591b3ed3435
kai <sea.k.naka@gmail.com>
2024-06-08 11:40:21 +0900

    official not success

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class UserController extends Controller
+{
+    public function show(User $user)
+    {
+        return Inertia::render('User/Show', [
+            'user' => $user
+        ]);
+    }
+}
